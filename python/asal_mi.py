be2751718191451eff680fa9672f10a52a96a930
Mehmet Tugrul Durmus <mehmettugruld11@gmail.com>
2025-02-24 18:45:51 +0300

    hata düzeltildi

--- python/asal_mi.py
+++ python/asal_mi.py
@@ -2,27 +2,27 @@ print("Asal sayılar sadece kendine ve 1' e bölünebile sayılardır")
 
 def asal_mi(sayi):
     if sayi < 2:
-        return False,[]
+        return False, []
     
     carpanlar = []
     carpanlar.append(1)
     carpanlar.append(sayi)
+
     for i in range(2, int(sayi ** 0.5) + 1):
-        if sayi %i == 0:
+        if sayi % i == 0:
             carpanlar.append(i)
             if sayi // i != i:
                 carpanlar.append(sayi // i)
-        
-    if carpanlar:
-        return False , sorted(carpanlar)
     
-    return True , []
+    if len(carpanlar) > 2:  # Eğer çarpanlar listesi 2'den fazla eleman içeriyorsa, sayı asal değildir.
+        return False, sorted(carpanlar)
+    
+    return True, []
 
 while True:
-
     sayi = int(input("Asal olup olmadığını öğrenmek istediğiniz sayıyı girin:\n "))
 
-    asal , carpanlar = asal_mi(sayi)
+    asal, carpanlar = asal_mi(sayi)
     
     if asal:
         print(f"{sayi} sayısı bir asal sayıdır.")
@@ -35,4 +35,5 @@ while True:
     if tekrar == "e":
         continue
     else:
+        print("Çıkış yapılıyor...")
         break

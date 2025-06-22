28917687ab292e8e822107eda35ca398af656a3a
Mehmet Tugrul Durmus <mehmettugruld11@gmail.com>
2025-06-22 23:36:11 +0300

    Kod eklendi

--- python /us_alma.py
+++ python /us_alma.py
@@ -1,6 +1,9 @@
 taban = int(input("Üssünü almak istediğiniz sayının tabanını giriniz: "))
 us = int(input("Tabanını girdiğiniz sayının üssünü giriniz: "))
 
-sonuc = pow(taban, us)
+sonuc = taban
+
+for i in range(us - 1):
+    sonuc *= taban
 
 print(f"{taban}^{us} = {sonuc}")
